44800cdf35716b7259bbfd855f7f44b55681d216
youkaisteve <youkaisteve@gmail.com>
2016-09-30 09:28:24 +0800

    add api version test

--- src/api/service/controllers/account.js
+++ src/api/service/controllers/account.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var routerV2 = express.Router();
 
 var util = require('util');
 
@@ -14,9 +15,22 @@ var requireAuth = libs.middleware.auth_service();
 
 module.exports = function (app) {
     app.use('/v1/account', router);
+    app.use('/v2/account', router,routerV2);
 };
 
+/*V1版本,获取账户信息,可通过v1/account/id 或者 v2/account/id 来访问*/
 router.get("/:id", requireAuth, function (req, res, next) {
+    return Q().then(function () {
+        return accountBL.test(req.params.id);
+    }).then(function (data) {
+        res.send(helper.buildSuccessResult(data));
+    }).catch(function (err) {
+        next(err);
+    })
+});
+
+/*V2版本新增接口,获取账户所有信息,可通过v2/account/full/id 来访问。v1/account/full/id 将导致404*/
+routerV2.get("/full/:id",requireAuth, function (req, res, next) {
     return Q().then(function () {
         return accountBL.test(req.params.id);
     }).then(function (data) {
